14ac828c2691478408499301dd0ffcfc192cb04d
Artem <ibnajub@gmail.com>
2022-10-19 19:59:49 +0300

    task 1256:fggfgf fgfg

---
+++ Test2.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+
+print("zfdsfsd")
+count = 1.5
+
+while count < 5:
+    count += 1
+    print(count)
+count = 1.5 + count
+count = 10
+count = count + 1.5
+print(count)
+
