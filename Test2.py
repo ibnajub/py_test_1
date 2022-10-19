e42a6bfe79d65e5bad393c9986f4bbb91e25adb8
Artem <ibnajub@gmail.com>
2022-10-19 20:06:11 +0300

    dfsdf dsdfdsq

--- Test2.py
+++ Test2.py
@@ -5,6 +5,7 @@ from typing import List
 print("zfdsfsd")
 count = 1.5
 
+
 while count < 5:
     count += 1
     print(count)
@@ -12,4 +13,5 @@ count = 1.5 + count
 count = 10
 count = count + 1.5
 print(count)
+#cvcvx
 
